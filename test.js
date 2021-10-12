e4a5f1e66ae27f3f28ebc13e3fdbbacd755805c3
bag_error <88831850+Erghel@users.noreply.github.com>
2021-10-12 21:48:06 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 const finalGrade = (exam, projects) => {
    
     if (exam > 90 || projects > 10) {
-        return 100;
+        return 100; // вы сдали экзамен ана 100 баллов (оценка 5) 
     } else if (exam > 75 && projects >= 5) {
-        return 90;
+        return 75; // вы сдали экзамен на 75 баллов (оценка 4) 
     } else if (exam > 50 && projects >= 2) {
-        return 75;
+        return 50; // вы сдали экзамен на 50 баллов (оценка 3)
     } else {
-        return 0;
+        return 0; // вы не сдали экзамен (оценка 2) - пересдача 
     }
    
 };
