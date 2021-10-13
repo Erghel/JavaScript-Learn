fcbbb49a6f749e426d2be667eb87153ef7f02564
bag_found <88831850+Erghel@users.noreply.github.com>
2021-10-13 20:56:30 +0300

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,7 +1,7 @@
 // Обычная функция
 const penSize = (yoursize) => {
   return 1.5 * yoursize;
-};
+}
 
 console.log(penSize(x)); // Вместо X ставим размер вашей ручки! 
                          // Формула по размеру ручки выдумана! 
