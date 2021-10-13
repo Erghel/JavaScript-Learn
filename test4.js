378d7e62a0bda7189627ffbea49a298dc3193236
bag_found <88831850+Erghel@users.noreply.github.com>
2021-10-13 20:57:45 +0300

    Update test4.js

--- test4.js
+++ test4.js
@@ -1,5 +1,6 @@
-const CelsCalc = (fahr) => { //
+// Переводчик градусов с Фаренгейта по Цельсию 
+const CelsCalc = (fahr) => { // fahr - Фаренгейты 
    return fahr - 32 * 5/9; 
 }
 
-cosnole.log(CelsCalc(x)); //
+cosnole.log(CelsCalc(x)); // Вместо X ставим градусы по Фаренгейту
