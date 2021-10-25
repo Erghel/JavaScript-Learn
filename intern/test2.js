245e3f77ef579e2ab8e1cb76da7ccf8fc671761c
Teaoctus <88831850+Erghel@users.noreply.github.com>
2021-10-25 23:35:00 +0300

    Create test2.js

---
+++ intern/test2.js
@@ -0,0 +1,12 @@
+let hello = 'Hello world!';
+
+let message;
+/* Мы создали 2 переменные hello со значением "Hello World" и переменная messege без значения */
+
+// Теперь мы копируем значение в messege из hello 
+message = hello;
+
+// теперь две переменные содержат одинаковые данные
+alert(hello); // Hello world!
+alert(message); // Hello world!
+//Теперь, чтобы мы не выбрали, нам выведет Hello World, так как мы сделали так, что там 2 одинаковых значения
