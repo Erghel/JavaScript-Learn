87628067d62edadaf6f0d7d891340a48a1a39e54
Teaoctus <88831850+Erghel@users.noreply.github.com>
2021-10-26 21:21:08 +0300

    Create test4.js

---
+++ intern/test4.js
@@ -0,0 +1,9 @@
+// Константы верхнего регистра 
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...когда нам нужно выбрать цвет мы объявим переменную color со значением из COLOR_ORANGE
+let color = COLOR_ORANGE;
+alert(color); // Выведет #FF7F00
