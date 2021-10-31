7edcadfcf1ecc3c8a26edeec4830919932963891
Teaoctus <88831850+Erghel@users.noreply.github.com>
2021-10-31 21:17:49 +0300

    Create test12.js

---
+++ test12.js
@@ -0,0 +1,14 @@
+// Проверка пароля перед входом на сайт (simple)
+
+let password = "1234";
+
+function checkPassword(inputPassword) {
+  return password === inputPassword;
+}
+
+if (checkPassword(prompt("Enter password"))) // Вместо inputPassword, здесь его значение будет равно тому, которое мы введем в всплывающем окне 
+  alert("Пароль верный");
+} 
+else {
+  alert("Пароль неверный, попробуй еще");
+}
