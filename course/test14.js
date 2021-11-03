8b48a2564308b5fba5e95efae5ac1eb27e97848f
Otto_octavius <88831850+Erghel@users.noreply.github.com>
2021-11-03 17:23:09 +0300

    Create test14.js

---
+++ course/test14.js
@@ -0,0 +1,11 @@
+const name = "Kazemir"
+const age = 14;
+console.log("His name is " + name + " and his age is " + age);
+
+// His name is Kazemir; and his age is 14, мы можем выводить приветствие вот так, но есть и еще один метод 
+
+/** let name = "Kazemir;
+*  let age = 14;
+* 
+* alert (`His name is ${name} and his age is ${age}`);
+*/
