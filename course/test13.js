85a6e2b52b043b20f6f0e1bb4801009ea4a8785b
Otto_octavius <88831850+Erghel@users.noreply.github.com>
2021-11-03 17:17:45 +0300

    Create test13.js

---
+++ course/test13.js
@@ -0,0 +1,7 @@
+const str = "There is a tab \t and here \ncomes the new line!"
+
+// Here is a tab    and here 
+// comes the new line!
+// \t значит большой пробел 
+// \n возле слова значит перенос на следующую строку 
+// также, если мы хотим поставить \, то мы должны огородить его вторым слешем \\ <-- получится так
