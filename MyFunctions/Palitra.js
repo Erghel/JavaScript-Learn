4b67d7b40a1d9353f47f685ea2cd5f9613960521
Otto_octavius <88831850+Erghel@users.noreply.github.com>
2021-11-09 18:41:55 +0300

    Create Palitra.js

---
+++ MyFunctions/Palitra.js
@@ -0,0 +1,46 @@
+// Вместо color1 и color2 пишем 2 значения обязательно используя "..." (как снизу в примере вывода в консоль) и сокращая название до 1 буквы
+// Пока в палитре мало цветов, вот их список:
+/**
+* к - красный
+* с - синий
+* ж = желтый
+* 
+*
+*
+*
+*
+*
+*/
+// Палитра будет упрощаться
+
+
+const Pal = (color1, color2) => {
+   
+    if (color1 == "к" && color2 == "с") {
+        return "Фиолетовый";  
+    }  else if (color1 == "с" && color2 == "к") {
+        return "Фиолетовый";  
+    }  else if (color1 == "к" && color2 == "к") {
+        return "Красный";  
+    }  else if (color1 == "к" && color2 == "к") {
+        return "Красный";  
+    }  else if (color1 == "к" && color2 == "ж")  {
+        return "оранжевый"; 
+    } else if (color1 == "ж" && color2 == "к")  {
+        return "оранжевый";
+    } else if (color1 == "ж" && color2 == "ж")  {
+        return "желтый"; 
+    } else if (color1 == "ж" && color2 == "ж") {
+        return "желтый"; 
+    } else if (color1 == "с" && color2 == "ж") {
+        return "зеленый"; 
+    } else if (color1 == "ж" && color2 == "с")  {
+        return "зеленый"; 
+    } else if (color1 == "с" && color2 == "ж")  {
+        return "зеленый"; 
+    } else {
+      return "Введите релевантное значение";
+    } 
+}
+
+console.log(Pal("x", "y"))
