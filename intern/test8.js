7e292a086b9a584dbc81f437f317135c07a5fe3d
bag_found <88831850+Erghel@users.noreply.github.com>
2021-11-18 21:30:05 +0300

    Update test8.js

--- intern/test8.js
+++ intern/test8.js
@@ -1,6 +1,6 @@
 // Выбор элемента страницы 
 document.querySelector('. /**Класс, который мы хотим выбрать*/ ')
-let /**Класс, который мы хотим выбрать*/ = document.querySelector('.X') // Вместо X мы ставим название CSS класса, который мы хотим использовать в скрипте
+let /**Название переменной*/ = document.querySelector('.X') // Вместо X мы ставим название CSS класса, который мы хотим использовать в скрипте
 
 let clickButton = document.querySelector('.cook');
 themeButton.onclick = function() {
