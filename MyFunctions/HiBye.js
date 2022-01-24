4e98bce37f62706096ccaccbca9fe69b969eede3
Erghel <88831850+Erghel@users.noreply.github.com>
2022-01-24 15:38:33 +0300

    Update HiBye.js

--- MyFunctions/HiBye.js
+++ MyFunctions/HiBye.js
@@ -5,7 +5,7 @@ function HiBye() {
     let here = confirm("Тут?");
     const answer = here; // Запишем ответ в константу 
 
-    if (answer == true) { // Если ответ 'Да', то нас приветсвуют
+    if (answer == true) { // Если ответ 'Да', то нас приветствуют
     alert("Привет");
 }
     else {
