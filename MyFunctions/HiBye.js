de31693c36a56e097f653a13cbacebc67a54dbd8
Erghel <88831850+Erghel@users.noreply.github.com>
2022-01-24 15:37:25 +0300

    Create HiBye.js

---
+++ MyFunctions/HiBye.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function HiBye() {
+
+    let here = confirm("Тут?");
+    const answer = here; // Запишем ответ в константу 
+
+    if (answer == true) { // Если ответ 'Да', то нас приветсвуют
+    alert("Привет");
+}
+    else {
+        alert("Пока"); // Иначе с нами прощаются
+    }
+}
+button.onclick = HiBye;
