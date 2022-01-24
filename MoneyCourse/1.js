91a6454b005548071bf998f110e12fcd70e1a955
Erghel <88831850+Erghel@users.noreply.github.com>
2022-01-24 15:24:23 +0300

    Create 1.js

---
+++ MoneyCourse/1.js
@@ -0,0 +1,23 @@
+"use strict";
+let name = "Anton"
+let bool = false; 
+let number = 4.6;
+
+
+const obj = {
+    name: "Ivan",
+    age: 25,
+    isMarried: true,
+    job: "Driver"
+};
+
+// console.log(obj.name); // Ivan
+// console.log(obj["name"]); // Ivan
+
+let arr = ["plum.png", "orange.jpeg", "apple.bmp", 6];
+
+console.log(arr[3]); // 6
+
+
+
+
