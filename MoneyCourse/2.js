6d3662a2383d218b94978a6112e3e1f3b22dc238
Erghel <88831850+Erghel@users.noreply.github.com>
2022-01-26 19:59:32 +0300

    Create 2.js

---
+++ MoneyCourse/2.js
@@ -0,0 +1,20 @@
+"use strict"
+
+// alert("Hello");
+
+// const result = confirm("Ты здесь?");
+// console.log(result); true - если ок, false - если отмена
+
+// const answer = +prompt("Вам есть 18?", ""); // + - определяет введенное значение будь это String или Nu,ber
+// console.log(answer + 5); // 15, так как оператор + определит введеные цифры внутри как number, соответствено позволит сложить, если + не был, получилось бы 105
+---------------------------------------------------------------------
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "")
+// answers[1] = prompt("Как ваша фамилия", "")
+// answers[2] = prompt("Сколько вам лет?", "")
+
+// document.write(answers); // Выведет на страницу то, что мы ввели в prompt
+
+// console.log(typeof(answers)) // object
+// console.log(typeof(null)); // object - признанная всеми ошибка
