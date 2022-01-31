f0c02c7c3027b03556d46c1db3ae16b7838ea52a
Erghel <88831850+Erghel@users.noreply.github.com>
2022-01-31 17:36:53 +0300

    Create 4.js

---
+++ MoneyCourse/4.js
@@ -0,0 +1,67 @@
+"use strict"
+
+// console.log("arr" + "- object"); // arr- object
+// console.log("4" + "- object"); // 4- object
+// console.log(4 + + "- object"); // NaN
+// console.log(4 + +"5"); // 9 (Унарный плюс)
+----------------------------------------------------------
+// let incr = 10,
+//     decr = 10;
+
+// incr++; // Postfix
+// decr--; // P
+
+// console.log(incr); // 11
+// console.log(decr); // 9
+----------------------------------------------------------
+// let incr = 10,
+//     decr = 10;
+
+// ++incr; // Prefix
+// --decr; // Prefix
+
+// console.log(incr); // 11
+// console.log(decr); // 9
+----------------------------------------------------------
+// let incr = 10,
+//     decr = 10;
+// console.log(incr++); // 10
+// console.log(decr--); // 10
+
+// let incr = 10,
+//     decr = 10;
+// console.log(++incr); // 11
+// console.log(--decr); // 19
+----------------------------------------------------------
+// console.log(5%2); // 1
+// console.log(2*4 == 8); // true
+// console.log(2*4 == "8"); // true (не строгое сравнение)
+// console.log(2*4 === "8"); // false
+----------------------------------------------------------
+// const isChecked = true,
+//       isClose = true;
+
+// console.log(isChecked && isClose); // true
+----------------------------------------------------------
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked && isClose); // false
+----------------------------------------------------------
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked || isClose); // true
+----------------------------------------------------------
+// const isChecked = false,
+//       isClose = false;
+
+// console.log(isChecked || isClose); // false
+----------------------------------------------------------
+// const isChecked = false,
+//       isClose = false;
+
+// console.log(isChecked || !isClose); // true
+----------------------------------------------------------
+// console.log(2 + 2 * 2 === 8); // false
+// console.log(2 + 2 * 2 != 8); // true
