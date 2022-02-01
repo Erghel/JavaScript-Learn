06d68dc1c6d6beb2e990fb0af07547bf427b20f9
Erghel <88831850+Erghel@users.noreply.github.com>
2022-02-01 14:14:13 +0300

    Update js1.js

--- CopyPaste/js1.js
+++ CopyPaste/js1.js
@@ -4,24 +4,22 @@ var sum=0;
 var out="";
 var clear="";
 var text="";
-function cl1(name, price)
-{
-if(sessionStorage.getItem('ssum')!=null)
-{
-sum=parseInt(sessionStorage.getItem('ssum'));
-out=sessionStorage.getItem('sout');
+
+function cl1(name, price) {
+  if(sessionStorage.getItem('ssum')!=null) {
+    sum=parseInt(sessionStorage.getItem('ssum'));
+    out=sessionStorage.getItem('sout');
 }
 
-out +=name + " " + price + "\n";
-sum+= price;
-document.getElementById('text').value = out+sum;
-sessionStorage.setItem('ssum',sum);
-sessionStorage.setItem('sout',out);
+  out +=name + " " + price + "\n";
+  sum+= price;
+  document.getElementById('text').value = out+sum;
+  sessionStorage.setItem('ssum',sum);
+  sessionStorage.setItem('sout',out);
 
 }
 
-function cl()
-{
+function cl() {
 sum=0;
 out="";
 document.getElementById('text').value = clear;
