55d57f35389114fbb6f6a6424740e77b51236ef1
Erghel <88831850+Erghel@users.noreply.github.com>
2022-02-04 18:47:08 +0300

    Update CorrectNumber2.js

--- MyFunctions/CorrectNumber2.js
+++ MyFunctions/CorrectNumber2.js
@@ -6,6 +6,6 @@ function CorrectNumber() {
         document.getElementById("cr").textContent = "Молодец, правильный ответ"; // Находит p тэг с id = cr, и записывает в него "Молодец, правильный ответ"
     }
     else { 
-        document.getElementById("cr").textContent = `Правильный ответ ${correct}`; // Находит p тэг с id = cr, и записывает в него Правильный ответ 9
+        document.getElementById("cr").textContent = `Правильный ответ ${correct}`; // Находит p тэг с id = cr, и записывает в него "Правильный ответ 9"
     }
 }
