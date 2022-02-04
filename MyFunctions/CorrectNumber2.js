b7074c1203afcb9d59934cb9fca09a30002b03d4
Erghel <88831850+Erghel@users.noreply.github.com>
2022-02-04 15:10:38 +0300

    Create CorrectNumber2.js

---
+++ MyFunctions/CorrectNumber2.js
@@ -0,0 +1,11 @@
+function CorrectNumber() {
+    let answer = prompt("Сколько будет 3 умножить на 3?", "");
+    const correct = 9;
+
+    if (answer == correct) {
+        document.getElementById("cr").textContent = "Молодец, правильный ответ"; // Находит p тэг с id = cr, и записывает в него "Молодец, правильный ответ"
+    }
+    else { 
+        document.getElementById("cr").textContent = `Правильный ответ ${correct}`; // Находит p тэг с id = cr, и записывает в него Правильный ответ 9
+    }
+}
