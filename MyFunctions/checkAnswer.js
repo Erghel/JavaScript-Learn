26ba0987f64880afbd17baaaf87b863eda4bcb2c
Erghel <88831850+Erghel@users.noreply.github.com>
2022-03-30 22:23:58 +0300

    Update checkAnswer.js

--- MyFunctions/checkAnswer.js
+++ MyFunctions/checkAnswer.js
@@ -4,10 +4,11 @@ for (let i = 0; i < 2; i++) {
     let a = +prompt("Введите число:", "");
     let b = +prompt("Введите число:", "");;
 
-    if (a && b < 10){
-        console.log("Ошибок нет"), console.log(a+b); 
+    if (a && b < 10 && a && b != null){
+        console.log("Ошибок нет"), console.log(a+b);
     } else {
         console.log("Ошибка");
+        i--; // Если в первом нажмут отмена, то цикл попросит ввести числа еще раз
     } 
 
 }
