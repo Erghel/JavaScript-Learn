604699a6f2b96c3b1704a0dddb5b497f0834608a
Erghel <88831850+Erghel@users.noreply.github.com>
2022-06-27 14:40:27 +0300

    Create iframe_del.js

---
+++ CopyPaste/iframe_del.js
@@ -0,0 +1,9 @@
+// Скрипт для удаления iframe YouTube окон на страницах 
+let iframe = $("iframe");
+
+if(iframe.attr("src").split("youtube").length > 1) {
+  iframe.remove();
+}
+
+// Для использования этого скрипта в расширении - понадобится подключить библиотеку jQuery 
+  
